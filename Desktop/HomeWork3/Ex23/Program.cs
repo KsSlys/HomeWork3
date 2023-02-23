b7fbac4be9aa2d79e2872fbf12b40fb8076187ec
«Ksenia» <ksenia_slys@mail.ru>
2023-02-23 22:47:16 +0300

    Ex19,Ex21,Ex23

---
+++ Desktop/HomeWork3/Ex23/Program.cs
@@ -0,0 +1,26 @@
+﻿// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+// * при N < 0, нужно вывести от N до -1
+// 3 -> 1, 8, 27
+// 5 -> 1, 8, 27, 64, 125
+
+Console.WriteLine("Введите число");
+int N = int.Parse(Console.ReadLine());
+int a = 1;
+int m = N;
+Console.Write($"{N} - > ");
+if (N < 0){
+    N = m;
+    N = -1;
+    while ( m <= N){
+    double result = Math.Pow(m,2);
+    Console.Write($"{result}, ");
+    m++;
+    }
+    
+}else {
+    while ( a <= N){
+    double result = Math.Pow(a,2);
+    Console.Write($"{result}, ");
+    a++;
+    }
+} 
